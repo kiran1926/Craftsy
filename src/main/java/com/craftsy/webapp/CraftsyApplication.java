f4b3c613cdc767f51e9f9b7fce87cca6fdffbae6
Kiran Roge <kiranroge19@gmail.com>
2024-12-18 10:50:15 -0600

    Initial commit

---
+++ src/main/java/com/craftsy/webapp/CraftsyApplication.java
@@ -0,0 +1,13 @@
+package com.craftsy.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/craftsy/webapp/CraftsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftsy.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
